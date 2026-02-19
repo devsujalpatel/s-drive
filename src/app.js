da71e46cd4b8cfff92a871aa048fad8719ce3100
Sujal Patel <sujal7455@gmail.com>
2026-02-19 23:50:20 +0530

    different http methods

--- src/app.js
+++ src/app.js
@@ -9,12 +9,59 @@ app.use(express.json());
 
 app.get("/", (req, res, next) => {
   try {
-    throw new Error("Mene Pheka error ")
     res.send("Hello World! 😀")
   } catch (error) {
     next(error)
   }
 });
+
+app.post("/user", (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            throw new Error("Email and Password is required");
+        }
+        res.status(201).json("User Logged in")
+    } catch (error) {
+        next(error)
+    }
+});
+
+
+app.patch("/user:id", (req, res, next) => {
+    try {
+        const {email} = req.body; 
+        const {id} = req.params.id;
+
+        if(!email) {
+            throw new Error("Email is not available")
+        }
+
+        if(!id) {
+            throw new Error("User Id is required")
+        }
+
+        res.status(200).json("User Updated successfully")
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+app.delete("/user:id", (req, res, next) => {
+    try {
+        const {id} = req.params.id;
+
+        if(!id) {
+            throw new Error("User Id is required")
+        }
+        // Deletion Logic
+
+        res.status(200).json("User deleted successfully")
+    } catch (error) {
+        next(error)
+    }
+})
 
 app.use((err, req, res, next) => {
     res.send(err.message)
