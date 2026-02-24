27d02afba7d6b8ce44b73f97056931673e74cd4f
Sujal Patel <sujal7455@gmail.com>
2026-02-24 23:43:51 +0530

    bug fix

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,23 @@ export const app = express();
 
 app.disable("x-powered-by");
 app.use(express.json());
+
+// Enabling Cors from anywhere
 app.use((req, res, next) => {
   res.set("Access-Control-Allow-Origin", "*")
   next()
 })
 
-// app.use(express.static("storage"))
 
+// servering files
 app.use((req, res, next) => {
-  if(req.query.action = "download") {
-    res.set('Content-Disposition', "attatchment")
+  if(req.query.action === "download") {
+    res.setHeader('Content-Disposition', "attachment")
   }
   express.static("storage")(req, res, next);
 })
 
+// serving directory content
 app.get("/", async (req, res) => {
   const filesList = await readdir("./storage")
   console.log(filesList)
